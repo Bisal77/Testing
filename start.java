333d7a2631a3b5cbd2803c43ea29b0579c707e2b
Bisal77 <129731861+Bisal77@users.noreply.github.com>
2023-04-03 13:44:16 +0530

    Just a casual Hello world
    U know what it is.

---
+++ start.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      String x="Hello ";
+      String y="World";
+      String z=x+y;
+
+      System.out.println(z);
+    }
+}
